7dc12801a37c12bed6c83651f2f243e0279f0275
田中賢治 <key.m4096.kung.fu@gmail.com>
2017-02-11 17:51:10 +0900

    Initial Commit

---
+++ DotsSwiftTwitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DotsSwiftTwitter
+//
+//  Created by 田中賢治 on 2017/02/11.
+//  Copyright © 2017年 田中賢治. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
