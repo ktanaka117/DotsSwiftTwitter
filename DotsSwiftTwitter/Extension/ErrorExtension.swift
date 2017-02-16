a980996a5e68881b495e7847992db11dd86a5746
Yutaro Muta <muta.yutaro@gmail.com>
2017-02-16 13:45:28 +0900

    Fix typo

--- DotsSwiftTwitter/Extension/ErrorExtension.swift
+++ DotsSwiftTwitter/Extension/ErrorExtension.swift
@@ -11,7 +11,7 @@ import Foundation
 extension Error {
   
   var typeMessage: String {
-    return "\type of: \(type(of: self)), error: \(self)"
+    return "type of: \(type(of: self)), error: \(self)"
   }
   
 }
